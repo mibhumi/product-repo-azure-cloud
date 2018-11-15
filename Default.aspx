25f29a76f24dffea8b0d492e3432d7edec7ab899
mibhumi <bhumipatel1196@gmail.com>
2018-11-15 16:02:03 +0530

    inti commit

---
+++ Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Id<asp:TextBox ID="id" runat="server"></asp:TextBox>
+            <br />
+            Product Name<asp:TextBox ID="name" runat="server"></asp:TextBox>
+            <br />
+            Product Image<asp:FileUpload ID="image" runat="server" />
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        String s = "Server=tcp:productserver.database.windows.net,1433;Initial Catalog=productDB;Persist Security Info=False;User ID=bhumi;Password=Amsterdam@143;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        SqlConnection conn = new SqlConnection(s);
+
+        string pid = id.Text;
+        string pname = name.Text;
+        string file = image.FileName;
+
+        String query = "INSERT INTO product (pid,pname,pimage) VALUES ('" + pid + "','" + pname + "','"  + file + "')";
+
+        conn.Open();
+        SqlCommand cmd = new SqlCommand(query, conn);
+        int chk = cmd.ExecuteNonQuery();
+        if (chk > 0)
+        {
+            id.Text = "";
+            name.Text = "";
+
+        }
+        conn.Close();
+    }
+}
